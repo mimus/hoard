a671d3319f97e197ff40639c6afe00cf3b8d74f2
Mimus <mimus@mus.org.uk>
2018-10-08 00:12:07 +0100

    Etherscan API - fetch actual gas spent on transaction

--- src/services/eth/etherscan.js
+++ src/services/eth/etherscan.js
@@ -28,43 +28,57 @@ var fetchTransaction = function (transactionId) {
     throttleEtherscanFetch(() => {
       axios.get(url).then((response) => {
         var x = response.data.result
-        var gas = u.newBigNumberForAsset(x.gas, asset) // gas and gas price is a hex string.
-        var gasPrice = u.newBigNumberForAsset(x.gasPrice, asset)
-        var fee = gas.times(gasPrice).div(10e17)
-        var valueReceived = u.newBigNumberForAsset(x.value, asset).div(10e17) // x.value is a hex string.
-        var inputAddresses = [x.from]
-        var inputs = [{
-          address: x.from,
-          value: valueReceived.plus(fee).toString()
-        }]
-        var outputs = [x.to].map(outputAddress => {
-          var outAddresses = [outputAddress]
-          return {
-            addresses: outAddresses,
-            value: valueReceived.toString(),
-            isToSingleAddress: outAddresses && outAddresses.length === 1
-          }
-        })
+        // now fetch the transaction receipt to see how much gas was actually used
+        var receiptUrl = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionReceipt&txhash=${transactionId}&apikey=${apiToken}`
 
-        // Now need to fetch the block so we can find the timestamp...
-        var blockNo = x.blockNumber / 1 // x.blockNumber is a hex string: convert to number
-        var blockUrl = `https://api.etherscan.io/api?module=block&action=getblockreward&blockno=${blockNo}&apikey=${apiToken}`
         throttleEtherscanFetch(() => {
-          axios.get(blockUrl).then((blockResponse) => {
-            var transaction = {
-              transactionId: x.hash,
-              date: new Date(blockResponse.data.result.timeStamp * 1000),
-              inputAddresses,
-              inputs,
-              outputs,
-              fee: fee.toString()
-            }
-            resolve(transaction)
+          axios.get(receiptUrl).then((receiptResponse) => {
+            var receipt = receiptResponse.data.result
+            var gasUsed = receipt.gasUsed // gasUsed is a hex string
+            var gas = u.newBigNumberForAsset(gasUsed, asset)
+            var gasPrice = u.newBigNumberForAsset(x.gasPrice, asset) // gasPrice is a hex string.
+            var fee = gas.times(gasPrice).div(10e17)
+            var valueReceived = u.newBigNumberForAsset(x.value, asset).div(10e17) // x.value is a hex string.
+            var inputAddresses = [x.from]
+            var inputs = [{
+              address: x.from,
+              value: valueReceived.plus(fee).toString()
+            }]
+            var outputs = [x.to].map(outputAddress => {
+              var outAddresses = [outputAddress]
+              return {
+                addresses: outAddresses,
+                value: valueReceived.toString(),
+                isToSingleAddress: outAddresses && outAddresses.length === 1
+              }
+            })
+
+            // Now need to fetch the block so we can find the timestamp...
+            var blockNo = x.blockNumber / 1 // x.blockNumber is a hex string: convert to number
+            var blockUrl = `https://api.etherscan.io/api?module=block&action=getblockreward&blockno=${blockNo}&apikey=${apiToken}`
+            throttleEtherscanFetch(() => {
+              axios.get(blockUrl).then((blockResponse) => {
+                var transaction = {
+                  transactionId: x.hash,
+                  date: new Date(blockResponse.data.result.timeStamp * 1000),
+                  inputAddresses,
+                  inputs,
+                  outputs,
+                  fee: fee.toString()
+                }
+                resolve(transaction)
+              }, (error) => {
+                console.error(error)
+                reject(error && error.message)
+              })
+            })
+
           }, (error) => {
             console.error(error)
             reject(error && error.message)
           })
         })
+
       }, (error) => {
         console.error(error)
         reject(error && error.message)
