a3a0bb44f7678e979dedfcf76f9d37a86b6eec65
Mimus <mimus@mus.org.uk>
2024-04-03 12:16:12 +0100

    Use new token address for USDC

--- src/services/usdc/index.js
+++ src/services/usdc/index.js
@@ -2,7 +2,8 @@ import polygonscanTokenService from '../matic/polygonscan_token'
 import maticvigil from '../matic/maticvigil'
 import covalenthq from '../matic/covalenthq'
 
-const maticTokenAddress = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+// const maticTokenAddress = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'  // original
+const maticTokenAddress = '0x3c499c542cef5e3811e1192ce70d8cc03d5c3359' // new
 
 export default {
   services: [ polygonscanTokenService(maticTokenAddress), maticvigil, covalenthq ],
